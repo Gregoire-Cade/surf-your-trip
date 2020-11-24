72f0f8573033d65bd5317ffe541828c0587aeefa
gregoire cade <gregoirecade.fr@gmail.com>
2020-11-24 12:06:49 +0000

    Create Spots & Trips controllers / Defining  Routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Spot.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+gregoire = User.create(nickname: "greg", location: "152 Bethnal Green Road", email: "gregoirecade.fr@gmail.com", password: "password")
+karolina =  User.create(nickname: "karo", location: "67 Palmerston Road", email: "karolinalangner1@gmail.com", password: "123456")
+
+puts "Finished!"
+
+# puts "Creating spots..."
+file1 = URI.open('https://www.surfholidays.com/assets/images/blog/2015-09-08-Trestles.jpg')
+cap_ferret = Spot.create(name: "Cap Ferret", location: "Lège-Cap-Ferret", level: "improver", user: gregoire)
+cap_ferret.photos.attach(io: file1, filename: '2015-09-08-Trestles.jpg', content_type: 'image/jpg')
+
+file2 = URI.open('https://www.surfertoday.com/images/stories/hossegor.jpg')
+koszalin = Spot.create(name: "Koszalin", location: "Dabki", level: "beginner", user: karolina)
+koszalin.photos.attach(io: file2, filename: 'hossegor.jpg', content_type: 'image/jpg')
+
+puts "Finished!"
