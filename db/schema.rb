e52bdd57b592a88ecb5e3ac77de3dc9eb0c73db3
gregoire cade <gregoirecade.fr@gmail.com>
2020-11-25 10:56:55 +0000

    Adding country to spot +  feeding  the seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_182355) do
+ActiveRecord::Schema.define(version: 2020_11_25_105317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_182355) do
     t.string "beach_type"
     t.text "about_location"
     t.string "punchline"
+    t.string "country"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,12 @@ puts "Finished!"
 # puts "Creating spots..."
 file1 = URI.open('https://www.surfholidays.com/assets/images/blog/2015-09-08-Trestles.jpg')
 file2 = URI.open('https://www.surfertoday.com/images/stories/hossegor.jpg')
-cap_ferret = Spot.create(name: "Cap Ferret", location: "Lège-Cap-Ferret", level: "improver", user: gregoire, description: "Cap Ferret is a smart seaside resort at the tip of the Cap Ferret peninsula that separates the Atlantic Ocean from the Bay of Arcachon. Hidden among pine trees with its fishermen and oyster farmers in their traditional wooden huts, Cap Ferret harks back to a bygone era.", beach_type: "sandy", about_location: "blablabalbalbabla", punchline: "Try local oysters, you won't regret")
+cap_ferret = Spot.create(name: "Cap Ferret", location: "Lège-Cap-Ferret", country: "France", level: "improver", user: gregoire, description: "Cap Ferret is a smart seaside resort at the tip of the Cap Ferret peninsula that separates the Atlantic Ocean from the Bay of Arcachon. Hidden among pine trees with its fishermen and oyster farmers in their traditional wooden huts, Cap Ferret harks back to a bygone era.", beach_type: "sandy", about_location: "blablabalbalbabla", punchline: "Try local oysters, you won't regret")
 cap_ferret.photos.attach(io: file1, filename: '2015-09-08-Trestles.jpg', content_type: 'image/jpg')
 cap_ferret.photos.attach(io: file2, filename: 'hossegor.jpg', content_type: 'image/jpg')
 
 file2 = URI.open('https://www.surfertoday.com/images/stories/hossegor.jpg')
-koszalin = Spot.create(name: "Koszalin", location: "Dabki", level: "beginner", user: karolina, description: "Dabki (Koszalin), Leba and Wladyslawowo are also quite interesting beach breaks waiting for the Central European surfers. Sopot and Wladyslawowo are probably the most important wave riding centers in Poland.", beach_type: "sandy", about_location: "blablabalbalbabla", punchline: "Try local oysters, you won't regret")
+koszalin = Spot.create(name: "Koszalin", location: "Dabki", country: "Poland", level: "beginner", user: karolina, description: "Dabki (Koszalin), Leba and Wladyslawowo are also quite interesting beach breaks waiting for the Central European surfers. Sopot and Wladyslawowo are probably the most important wave riding centers in Poland.", beach_type: "sandy", about_location: "blablabalbalbabla", punchline: "Try local oysters, you won't regret")
 koszalin.photos.attach(io: file2, filename: 'hossegor.jpg', content_type: 'image/jpg')
 
 review1 = Review.create(user: gregoire, title: "Best surf spot in South West of France", content: "This surf spot is really good for beginners as the wind does not go crazy!", rating: 4, spot: cap_ferret)
