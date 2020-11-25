4eb2e18eb72a25694687905b3c6f51455e35e672
karolina langner <karolinalangner1@gmail.com>
2020-11-25 15:55:31 +0000

    New form for users to add their spots

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-    resources :spots, only: [:index, :show] do
+    resources :spots, only: [:index, :show, :new, :create] do
       resources :trips, only: [:create]
     end
     resources :trips, only: [:index, :show, :destroy]
