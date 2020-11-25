1d860c80e0812de5c4b092c6dde17c3554bb1a3d
karolina langner <karolinalangner1@gmail.com>
2020-11-25 13:05:50 +0000

    trips controller + banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-    resources :spots, only: [:index, :show]
+    resources :spots, only: [:index, :show] do
+      resources :trips, only: [:create]
+    end
+    resources :trips, only: [:index, :show, :destroy]
 end
