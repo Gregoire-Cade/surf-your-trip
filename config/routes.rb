73a258168e97316e1bda1d2758ab12ec3e4cacb8
gregoire cade <gregoirecade.fr@gmail.com>
2020-11-26 13:11:21 +0000

    adding a review to a spot

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
     resources :spots, only: [:index, :show, :new, :create] do
-      resources :trips, only: [:create]
       resources :reviews, only: [:new, :create]
+      resources :trips, only: [:create]
     end
     resources :trips, only: [:index, :show, :destroy]
-    
+
 end
