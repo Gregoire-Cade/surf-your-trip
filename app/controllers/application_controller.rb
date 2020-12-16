1d81a85dd43b7515a0508346702700a1941c1f68
gregoire cade <gregoirecade.fr@gmail.com>
2020-12-16 10:07:25 +0000

    removing expire_hsts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_filter :expire_hsts
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -10,10 +9,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:location, :photo])
   end
 
-
-def expire_hsts
-  response.headers["Strict-Transport-Security"] = 'max-age=0'
-end
   # app/controllers/application_controller.rb
 
 def default_url_options
